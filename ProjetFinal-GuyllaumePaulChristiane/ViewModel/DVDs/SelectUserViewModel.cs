3c8b9850aa8969f7fdf02d2a4b4cb647c907f7f2
guyllaume beaudry <guyllaumebeaudry@gmail.com>
2024-09-18 16:12:45 -0400

    Ajout des multiples Index

---
+++ ProjetFinal-GuyllaumePaulChristiane/ViewModel/DVDs/SelectUserViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ProjetFinal_GuyllaumePaulChristiane.Models;
+
+namespace ProjetFinal_GuyllaumePaulChristiane.ViewModel.DVDs
+{
+    public class SelectUserViewModel
+    {
+        public string SelectedUserId { get; set; }
+        public List<SelectListItem> Users { get; set; } 
+    }
+}
---
+++ ProjetFinal-GuyllaumePaulChristiane/ViewModel/DVDs/SelectedUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ProjetFinal_GuyllaumePaulChristiane.Models;
+
+namespace ProjetFinal_GuyllaumePaulChristiane.ViewModel.DVDs
+{
+    public class SelectedUserViewModel
+    {
+        public string SelectedUserId { get; set; }
+        public string UserName { get; set; }
+        public int TotalPages { get; set; }
+        public int currentPage { get; set; }
+        public IEnumerable<DVD> DVDs { get; set; }
+    }
+}
