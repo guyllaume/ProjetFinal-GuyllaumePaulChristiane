5f4fa1b5a92601691f2333f543689b7f2315d8eb
Paulhm1 <105753579+Paulhm1@users.noreply.github.com>
2024-09-21 16:48:07 -0400

    épuratuion en-tête + redirection apres connexion

--- ProjetFinal-GuyllaumePaulChristiane/Program.cs
+++ ProjetFinal-GuyllaumePaulChristiane/Program.cs
@@ -45,7 +45,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=DVDs}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 
